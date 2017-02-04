a2d53842081597efe42f1a528b90f35d11c3d85a
Not-So-Lazy-Programmer <jeremyfettjr@gmail.com>
2017-02-04 13:14:43 -0800

    Add files via upload

---
+++ learning 01.java
@@ -0,0 +1,22 @@
+package tutorial;
+
+public class Learning01 {
+
+	public static void main(String[] args){
+	
+	int i = 0;
+	
+		
+	while(i< 1000000){
+	System.out.println(i);
+	i++;
+
+	
+	if (i >= 900000){
+	System.out.println("Its over 900,000!!!");
+	}
+	}
+	}
+	
+}
+
