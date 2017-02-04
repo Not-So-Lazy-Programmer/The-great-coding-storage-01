d9a053b32d0825a6ebf81d70f31a0eb5ad471d09
Not-So-Lazy-Programmer <jeremyfettjr@gmail.com>
2017-02-04 13:31:50 -0800

    Add files via upload

---
+++ learning 01.java
@@ -0,0 +1,21 @@
+package tutorial;
+
+public class Class {
+
+public static void main(String args []){
+	
+	int name = 1;
+	
+	while(name>0 && name <= 2000000){
+		if(name % 2 == 1){
+			System.out.println(name);
+			name++;
+		}
+	
+		else if (name % 2 == 0){
+	System.out.println(name);
+	name++;
+	}
+	}
+}
+}
