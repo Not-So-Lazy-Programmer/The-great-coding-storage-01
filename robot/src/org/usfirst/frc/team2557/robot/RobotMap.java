afdea75cfed5b7b6f52edcac77c8a699f9d95b45
jeremy fett <jeremyfettjr@gmail.com>
2017-02-10 18:23:03 -0800

    save this robot command

--- robot/src/org/usfirst/frc/team2557/robot/RobotMap.java
+++ robot/src/org/usfirst/frc/team2557/robot/RobotMap.java
@@ -1,4 +1,8 @@
 package org.usfirst.frc.team2557.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,6 +10,25 @@ package org.usfirst.frc.team2557.robot;
  * floating around.
  */
 public class RobotMap {
+	
+	public static RobotDrive robotdrive;
+	
+	public static CANTalon motorBL;
+	public static CANTalon motorBR;
+	public static CANTalon motorFL;
+	public static CANTalon motorFR;
+	
+	public static void init () {
+	
+		motorBL = new CANTalon(1);
+		motorBR = new CANTalon(2);
+		motorFL = new CANTalon(3);
+		motorFR = new CANTalon(4);
+		
+		robotdrive = new RobotDrive (motorBL,motorBR,motorFL,motorFR);
+		
+		
+		
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -15,4 +38,5 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+}
 }
---
+++ robot/src/org/usfirst/frc/team2557/robot/subsystems/subsystem.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team2557.robot.subsystems;
+
+import org.usfirst.frc.team2557.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class subsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void drive() {
+    	RobotMap.robotdrive.arcadeDrive(0,0);
+    	
+    	
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
